dfd3dc2a3c419726a1e7bdb628bd18ae4e28698a
r-takata-sdc <r_takata@sdcns.co.jp>
2021-06-10 10:21:46 +0900

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="サンプルページ"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
